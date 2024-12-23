fb49184c354b96ec3556d0458261e98c7a1b09a5
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2024-12-23 18:41:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ var donationAddress = "";
 var blockTargetInterval = 60; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 ZTC
 var totalSupply =  750000000000; // enter the total supply in atomic units
-var symbol = 'ZTC'; // enter the coin's ticker
+var symbol = 'ZTCH'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "ztc": [
+ "ztch": [
    ["superblockchain.con-ip.com", "https://superblockchain.con-ip.com:14036"]
  ]
 };
 
 var networkStat2 = {
-  "ztc": [
+  "ztch": [
 
  ]
 };
 
 var networkStat3 = {
-   "ztc": [
+   "ztch": [
      ["webztc.semipool.com", "https://webztc.semipool.com/api"]
   ]
 };
