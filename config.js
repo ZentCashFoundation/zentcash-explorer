6f3b9c20ec0b86b6c2cb0dbd5906aaf275f39e73
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2024-12-23 18:52:06 +0100

    Ticket ZTC --- ZTCH

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'https://superblockchain.con-ip.com/api/ztc';
 var donationAddress = "";
 var blockTargetInterval = 60; // enter the block interval in seconds
-var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 ZTC
+var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 ZTCH
 var totalSupply =  750000000000; // enter the total supply in atomic units
 var symbol = 'ZTCH'; // enter the coin's ticker
 var refreshDelay = 30000;
